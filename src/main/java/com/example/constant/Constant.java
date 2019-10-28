973d0ff4d5b1d6db624387e6504fe53fc255fbb0
088ashu <53143203+088ashu@users.noreply.github.com>
2019-10-28 20:31:01 +0530

    first

--- src/main/java/com/example/constant/Constant.java
+++ src/main/java/com/example/constant/Constant.java
@@ -3,6 +3,8 @@ package com.example.constant;
 public class Constant {
 	public static final String MODEL_KEY = "user";
 
+	public static final String MODEL_KEY1 = "user1";
+
 	public static final String FORM_LOGICAL_NAME = "userReg";
 
 	public static final String RESPONSE_MESSAGE = "responseMsg";
