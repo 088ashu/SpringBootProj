81f487fe588f8657536b5f1beb0f581ae187273d
Ashutosh Singh <53143203+088ashu@users.noreply.github.com>
2020-02-23 22:37:16 +0530

    new commit

--- src/main/java/com/example/constant/Constant.java
+++ src/main/java/com/example/constant/Constant.java
@@ -2,6 +2,8 @@ package com.example.constant;
 
 public class Constant {
 	public static final String MODEL_KEY = "user";
+	
+	public static final String MODEL_KEY1 = "user1";
 
 	public static final String MODEL_KEY1 = "user1";
 
